18f3bbf5ebb4acd848ac3b2ace698b425a173ac2
ctcarrier <chris.carrier@twintechs.com>
2019-04-04 21:23:34 -0400

    Fix up sample script

--- take-sample.py
+++ take-sample.py
@@ -5,13 +5,19 @@ from wrappers import GPhoto
 from wrappers import Identify
 from wrappers import NetworkInfo
 
+import subprocess
+import logging
+import signal
+
+self.camera = GPhoto(subprocess)
+
 try:
     self.camera.set_shutter_speed(sys.argv[1])
     self.camera.set_iso(iso=str(sys.argv[2]))
 except Exception, e:
     logging.info("Error setting configs")
 try:
-    filename = self.camera.capture_image_and_download(shot=self.shot, image_directory=TMP_DIRECTORY)
+    filename = self.camera.capture_image_and_download(shot=self.shot, image_directory='/var/lib/timelapse/preview/test.jpg')
 except Exception, e:
     logging.error("Error on capture." + str(e))
     print "Error on capture." + str(e)
