47d65b166b116ca60ba775a5657625d03f6b8457
ctcarrier <chris.carrier@twintechs.com>
2019-04-04 21:50:40 -0400

    Lose self

--- take-sample.py
+++ take-sample.py
@@ -9,18 +9,18 @@ import subprocess
 import logging
 import signal
 
-self.camera = GPhoto(subprocess)
+camera = GPhoto(subprocess)
 
 logging.basicConfig(filename='/var/log/timelapse.log', level=logging.INFO, format='%(asctime)s %(message)s')
 logging.info('Taking sample shot %s' % __file__)
 
 try:
-    self.camera.set_shutter_speed(sys.argv[1])
-    self.camera.set_iso(iso=str(sys.argv[2]))
+    camera.set_shutter_speed(sys.argv[1])
+    camera.set_iso(iso=str(sys.argv[2]))
 except Exception, e:
     logging.info("Error setting configs")
 try:
-    filename = self.camera.capture_image_and_download(shot=self.shot, image_directory='/var/lib/timelapse/preview/test.jpg')
+    filename = camera.capture_image_and_download(shot=0, image_directory='/var/lib/timelapse/preview/test.jpg')
 except Exception, e:
     logging.error("Error on capture." + str(e))
     print "Error on capture." + str(e)
